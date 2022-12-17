20402e9288e52cd98434f4c23fd1915b7fad072a
“Cláudio <claudiobmontenegro@gmail.com>
2022-12-17 11:40:47 -0300

    Add Project v1.0

---
+++ src/main/java/dev/fujioka/java/avancado/web/model/Aluno.java
@@ -0,0 +1,21 @@
+package dev.fujioka.java.avancado.web.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+@Getter
+@Setter
+@Entity
+public class Aluno {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String nome;
+
+    private String matricula;
+
+    @Transient
+    public String teste;
+}
